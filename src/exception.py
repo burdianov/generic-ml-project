cdeea202b7abc9d64e949943f2cff4ca5703c16d
Lilian Burdianov <lilian.burdianov@gmail.com>
2023-08-13 13:26:28 +0400

    data transformation

--- src/exception.py
+++ src/exception.py
@@ -5,9 +5,7 @@ def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
 
     error_message = "Error occurred in python script name: [{0}], line number: [{1}], error message: [{2}]".format(
-        exc_tb.tb_frame.f_code.co_filename,
-        exc_tb.tb_lineno,
-        str(error)
+        exc_tb.tb_frame.f_code.co_filename, exc_tb.tb_lineno, str(error)
     )
 
     return error_message
@@ -17,7 +15,8 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(
-            error_message, error_detail=error_detail)
+            error_message, error_detail=error_detail
+        )
 
     def __str__(self):
         return self.error_message
